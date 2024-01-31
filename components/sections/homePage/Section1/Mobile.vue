06eccb38b58b45f80eff42520387be271747bbfd
Camilo Ramirez <ancamiram@gmail.com>
2024-01-30 23:26:51 -0500

    responsive section 1 WIP

--- components/sections/homePage/Section1/Mobile.vue
+++ components/sections/homePage/Section1/Mobile.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="absolute right-0 block sm:hidden">
-    <div class="">
-      <ImageComponent src="/MaskHome.png" class="w-[100px] " :fontControlled="false" />
-      <p class="right-0 top-0 absolute text-center text-white text-xs font-semibold font-['Poppins'] leading-normal">
-        Nuestra Filosofia</p>
+  <div class="absolute w-full">
+    <div class="absolute right-0 block sm:hidden">
+      <div class="">
+        <ImageComponent src="/MaskHome.png" class="w-[160px] " :fontControlled="false" />
+        <p class="right-5 top-5 absolute text-center text-white text-xs font-semibold font-['Poppins'] leading-normal">
+          Nuestra Filosofia</p>
+      </div>
+    </div>
+    <div
+      class="btn btn-circle btn-active  bg-white shadow-xl w-40 h-5 flex flex-col justify-center items-center absolute right-0 top-32  ">
+      <p class="text-xs font-['Poppins']">Mas Informacion</p>
     </div>
   </div>
 </template>
