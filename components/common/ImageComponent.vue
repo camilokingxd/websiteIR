570f663bf1993468c22c674eb5be010d2155f96e
Camilo Ramirez <ancamiram@gmail.com>
2024-02-01 22:36:39 -0500

    image component

--- components/common/ImageComponent.vue
+++ components/common/ImageComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <NuxtImg sizes="320 sm:640 md:768 lg:1024 xl:1280 xxl:1536" loading="lazy" :src="src" :alt="alt" :width="width"
-    :height="height" :class="customClass" />
+    :height="height" :class="customClass" format="webp" />
 </template>
 
 <script lang="ts" setup>
